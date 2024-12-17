9ff18f9171031b81c2f169d8160117e0814a03c7
Thommas-yokgaraj <yokgarajthommas@gmail.com>
2024-12-17 12:51:29 +0530

    Add files via upload

---
+++ project/cloud_management/cloud_service/script.sh
@@ -0,0 +1,6 @@
+REPLACE quick brown fox jumps over REPLACE lazy dogs
+A journey of a thousand miles begins with a single steps
+To  err is human, to forgive drive 
+better late than never but never late is better 
+success usually comes to who are too busy to be looking for it 
+REPLACE early bird catches REPLACE worm,tbut the second mouse gets the chesse
